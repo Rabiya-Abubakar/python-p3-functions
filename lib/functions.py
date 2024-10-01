2198a6970c232434bed7e350e9a6b2a4ab382931
+ <+>
2024-10-01 14:40:34 +0300

    final-submission

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
-
+    print("Hello, programmer!")
+greet_programmer()
+        
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Guido!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(45, 59))
 
-def halve(number):
-    pass
+
+def halve(num1):
+    return num1 / 2
+print(halve(100))
+    
